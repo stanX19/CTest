df8477fd181d65ace1ef3cd1b8bdb4920b669c30
StanX19 <tanshanchien@gmail.com>
2024-01-11 09:13:41 +0800

    argv parse seperate

--- srcs/UnitTest.cpp
+++ srcs/UnitTest.cpp
@@ -1,52 +1,5 @@
 #include "UnitTest.hpp"
 
-static bool processOption(char c) {
-    using namespace UnitTestconfig;
-
-	switch (c)
-	{
-	case 'a':
-		showAll = true;
-		return 0;
-	case 'k':
-		showKO = true;
-		return 0;
-	case 'd':
-		showKODetails = true;
-		return 0;
-	}
-	return 1;
-}
-
-static bool processArgv(int argc, char** argv, std::string &error) {
-	for (int i = 1; i < argc; i++) {
-        std::string arg(argv[i]);
-        if (arg[0] != '-') 
-			return 1;
-		for (const char &c: arg.substr(1)) {
-			if (processOption(c) == 0)
-				continue;
-			error = std::string("-") + c;
-			return 1;
-		}
-	}
-	return 0;
-}
-
-bool UnitTestconfig::parseArgv(int argc, char** argv) {
-	std::string error;
-    if (processArgv(argc, argv, error)) {
-		std::cerr << "Error: Unrecognized option '" << error << "'\n";
-		std::cerr << "Usage: " << argv[0] << " [-a] [-k] [-d]\n"
-				  << "Options:\n"
-				  << "  -a : Show all test results\n"
-				  << "  -k : Show only failed test results\n"
-				  << "  -d : Show details of failed test cases\n";
-		return false; // Indicate error
-    }
-    return true; // Parsing successful
-}
-
 UnitTest::UnitTest(std::string directory, std::string CC, std::string CFLAGS)
     : directory_(directory), CC_(CC), CFLAGS_(CFLAGS) {}
 
